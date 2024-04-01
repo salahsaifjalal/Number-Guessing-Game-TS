b9dae130e34355185c29138bad5eed2083f76dac
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2024-04-01 16:01:11 +0500

    final update

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.bold.magentaBright("Welcome to CLI based Number Guessing Game!"));
-//let userEnter: boolean
 let computerGeneratedNumber = Math.floor(Math.random() * 21);
-const guessedNumber = await inquirer.prompt([
-    {
-        name: "userInput",
-        type: "number",
-        message: chalk.bold.greenBright("Enter the number from 0 to 20, you guessed!"),
-    },
+//console.log(computerGeneratedNumber)
+const askPlayAgain = await inquirer.prompt([
     {
         name: "userEnter",
         type: "string",
-        message: chalk.bold.greenBright("Would you like to play again! Press y for YES and n for NO"),
+        message: chalk.bold.greenBright("Would you like to play! Press y for YES and n for NO"),
     },
 ]);
-let myfunction = () => {
-    if (guessedNumber.userInput === computerGeneratedNumber) {
-        console.log(chalk.bold.blueBright("Congratulation! You Correctly guessed the number."));
-    }
-    else {
-        console.log(chalk.bold.redBright("Sorry! You could not guess the number. The number was ") + computerGeneratedNumber);
-    }
-};
-let myfunction2 = () => {
-    //console.log(guessedNumber.message);
-    //if (guessedNumber.userEnter === "y" || guessedNumber === "Y") {
-    //  myfunction();
-};
-myfunction();
-//myfunction2()
-//console.log(guessedNumber.message);
+switch (askPlayAgain.userEnter) {
+    case "n":
+        console.log(chalk.bold.bgRedBright("The game ends! "));
+        break;
+    case "y":
+        const guessedNumber = await inquirer.prompt([
+            {
+                name: "userInput",
+                type: "number",
+                message: chalk.bold.greenBright("Enter the number from 0 to 20, you guessed!"),
+            },
+        ]);
+        if (guessedNumber.userInput === computerGeneratedNumber) {
+            console.log(chalk.bold.blueBright("Congratulation! You Correctly guessed the number."));
+        }
+        else {
+            console.log(chalk.bold.redBright("Sorry! You could not guess the number. The number was ") + computerGeneratedNumber);
+        }
+        break;
+}
--- index.ts
+++ index.ts
@@ -5,47 +5,45 @@ console.log(
   chalk.bold.magentaBright("Welcome to CLI based Number Guessing Game!")
 );
 
-//let userEnter: boolean
 let computerGeneratedNumber = Math.floor(Math.random() * 21);
+//console.log(computerGeneratedNumber)
 
-const guessedNumber = await inquirer.prompt([
-  {
-    name: "userInput",
-    type: "number",
-    message: chalk.bold.greenBright(
-      "Enter the number from 0 to 20, you guessed!"
-    ),
-  },
-
+const askPlayAgain = await inquirer.prompt([
   {
     name: "userEnter",
     type: "string",
     message: chalk.bold.greenBright(
-      "Would you like to play again! Press y for YES and n for NO"
+      "Would you like to play! Press y for YES and n for NO"
     ),
   },
 ]);
 
-let myfunction = () => {
-  if (guessedNumber.userInput === computerGeneratedNumber) {
-    console.log(
-      chalk.bold.blueBright("Congratulation! You Correctly guessed the number.")
-    );
-  } else {
-    console.log(
-      chalk.bold.redBright(
-        "Sorry! You could not guess the number. The number was "
-      ) + computerGeneratedNumber
-    )
-  }
+switch (askPlayAgain.userEnter) {
+  case "n":
+    console.log(chalk.bold.bgRedBright("The game ends! "));
+    break;
+  case "y":
+    const guessedNumber = await inquirer.prompt([
+      {
+        name: "userInput",
+        type: "number",
+        message: chalk.bold.greenBright(
+          "Enter the number from 0 to 20, you guessed!"
+        ),
+      },
+    ]);
+    if (guessedNumber.userInput === computerGeneratedNumber) {
+      console.log(
+        chalk.bold.blueBright(
+          "Congratulation! You Correctly guessed the number."
+        )
+      );
+    } else {
+      console.log(
+        chalk.bold.redBright(
+          "Sorry! You could not guess the number. The number was "
+        ) + computerGeneratedNumber
+      );
+    }
+    break;
 }
-let myfunction2= () => { 
-//console.log(guessedNumber.message);
-//if (guessedNumber.userEnter === "y" || guessedNumber === "Y") {
-//  myfunction();
-}               
-
-
-myfunction();
-//myfunction2()
-//console.log(guessedNumber.message);
